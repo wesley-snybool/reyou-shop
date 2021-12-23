bf227cdf9de586d7c5daa1e8ff5e6e079bcd30c9
Wesley Paiva <wesley.paiva@euemdia.com.br>
2021-12-22 23:09:57 -0300

    finaly

--- src/pages/marcas/pagina-marca/index.tsx
+++ src/pages/marcas/pagina-marca/index.tsx
@@ -43,7 +43,7 @@ export default function PageBrand() {
                 })}
             </div>
             <Search showSearchBrand={false} title={'Produtos'} />
-            <CarrousselGallery />
+            {/* <CarrousselGallery /> */}
         </Container>
     )
 }
