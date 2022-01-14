a7835b2f7b000e52b22fa09d19e7b1014c61f436
Wesley Paiva <wesley.paiva@euemdia.com.br>
2022-01-14 20:02:05 -0300

    adjustment blog reyou

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -2,7 +2,9 @@ import Layout from "@components/layout/layout";
 
 export default function Blog () {
     return (
-        <div>Hello</div>
+        <div>
+            <h1 className="font-bold text-center text-2xl mt-10">Em breve Blog Reyou...</h1>
+        </div>
     )
 }
 
