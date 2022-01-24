48d4733415061d5a539084bcd5bf988aaa74dcc4
Wesley Paiva <wesley.paiva@euemdia.com.br>
2022-01-24 15:44:13 -0300

    integração reyou na imprensa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { useAppDispatch } from 'src/redux/store/store';
+import Image from "next/image";
+import Link from "next/link";
+import { useAppDispatch } from "src/redux/store/store";
 import Container from "@components/ui/container";
 import BannerCarouselBlock from "@containers/banner-carousel-block";
 import DownloadApps from "@components/common/download-apps";
@@ -23,16 +23,16 @@ import { useDispatch } from "react-redux";
 import { changeUser } from "src/redux/store/userSlice";
 import { getDarlingMoments } from "src/redux/modules/darlings-moment/darlingsMoments";
 import { getReyouFavorites } from "src/redux/modules/reyou-favorites/reYouFavorites";
-import { getNews } from 'src/redux/modules/news/news';
-import { getConfig } from 'src/redux/modules/config-portal/config-portal';
-import { getBlogs } from 'src/redux/modules/blogs/blogs';
-import { getFlipCard } from 'src/redux/modules/flip-cards/getFlipCardSlice';
-import { getBanner } from 'src/redux/modules/banners/getBannerSlice';
-import { getPublicity } from 'src/redux/modules/publicity/publicitySlice';
-import { getPress } from 'src/redux/modules/press/press';
-import { useConfigCompany } from 'src/redux/hooks/companyHooks';
-import { isEmpty } from 'lodash';
-import { ConfigPortal } from 'src/types/types';
+import { getNews } from "src/redux/modules/news/news";
+import { getConfig } from "src/redux/modules/config-portal/config-portal";
+import { getBlogs } from "src/redux/modules/blogs/blogs";
+import { getFlipCard } from "src/redux/modules/flip-cards/getFlipCardSlice";
+import { getBanner } from "src/redux/modules/banners/getBannerSlice";
+import { getPublicity } from "src/redux/modules/publicity/publicitySlice";
+import { getPress } from "src/redux/modules/press/press";
+import { useConfigCompany } from "src/redux/hooks/companyHooks";
+import { isEmpty } from "lodash";
+import { ConfigPortal } from "src/types/types";
 
 type BlogsType = {
 	image: string;
@@ -49,17 +49,27 @@ export default function Home() {
 	const [flipCardState, setFlipCardState] = useState([]);
 
 	//Recuperando os dados da Sessão queridinhos do momentos no redux
-	const { isLoading, error } = useAppSelector((state) => state.getDarlingMoments);
-	const dataDarlingMomentsRedux = useAppSelector((state) => state.getDarlingMoments.data);
+	const { isLoading, error } = useAppSelector(
+		(state) => state.getDarlingMoments
+	);
+	const dataDarlingMomentsRedux = useAppSelector(
+		(state) => state.getDarlingMoments.data
+	);
 
 	//Recuperando os dados da Sessão queridinhos do momentos no redux
-	const { isLoading: isLoadDFavorites, error: errorFavorites } = useAppSelector((state) => state.getReyouFavorites);
+	const { isLoading: isLoadDFavorites, error: errorFavorites } = useAppSelector(
+		(state) => state.getReyouFavorites
+	);
 	const dataFavorites = useAppSelector((state) => state.getReyouFavorites.data);
 
 	//Buscando os dados dos FlipsCards e guardando no reduxjs
-	const { data: dataFlips, isLoading: isLoadingFlipCard }: any = useAppSelector((state) => state.getFlipCardsData);
+	const { data: dataFlips, isLoading: isLoadingFlipCard }: any = useAppSelector(
+		(state) => state.getFlipCardsData
+	);
 
-	const { data: dataPress, isLoading: isLoadingPress } = useAppSelector((state) => state.getPress);
+	const { data: dataPress, isLoading: isLoadingPress } = useAppSelector(
+		(state) => state.getPress
+	);
 
 	const dataBlogs = useAppSelector((state) => state.getBlogs.data);
 
@@ -68,10 +78,10 @@ export default function Home() {
 	//Exemplo de dispatch
 	const handleChangeUser = () => {
 		const data = {
-			nome: 'Wesley',
-			logado: true
-		}
-		dispatch(changeUser(data.nome))
+			nome: "Wesley",
+			logado: true,
+		};
+		dispatch(changeUser(data.nome));
 	};
 
 	useEffect(() => {
@@ -87,13 +97,23 @@ export default function Home() {
 	}, []);
 
 	useEffect(() => {
-		if (!isLoading && !error.error_status && dataDarlingMomentsRedux.length > 0) {
+		if (
+			!isLoading &&
+			!error.error_status &&
+			dataDarlingMomentsRedux.length > 0
+		) {
 			setDarlingState(dataDarlingMomentsRedux);
 		}
-		if (isLoadingFlipCard === 'SUCCESS' && dataFlips.length > 0) {
+		if (isLoadingFlipCard === "SUCCESS" && dataFlips.length > 0) {
 			setFlipCardState(dataFlips);
 		}
-	}, [isLoading, error.error_status, isLoadingFlipCard, flipCardState, dataDarlingMomentsRedux]);
+	}, [
+		isLoading,
+		error.error_status,
+		isLoadingFlipCard,
+		flipCardState,
+		dataDarlingMomentsRedux,
+	]);
 
 	useEffect(() => {
 		dispatchApp(getPress());
@@ -107,101 +127,165 @@ export default function Home() {
 			<HeroBlock />
 
 			<Container className="px-1 md:px-0  w-full">
-				<div className='flipinf flex flex-col items-center justify-center'>
-					<strong className="mb-8"> Que tal associar valores às suas compras?</strong>
-					<h2 className="mb-8 text-black">Comece selecionando quais causas que te movem</h2>
+				<div className="flipinf flex flex-col items-center justify-center">
+					<strong className="mb-8">
+						{" "}
+						Que tal associar valores às suas compras?
+					</strong>
+					<h2 className="mb-8 text-black">
+						Comece selecionando quais causas que te movem
+					</h2>
 				</div>
 				<div className="container-main-flip-card">
 					{flipCardState?.map((item: any, index: number) => {
 						return (
-							<FlipCard widthImage={item.image.desktop.width} heightImage={310} titleFlip={item.title} options={item.options} key={`${index}--flips--cards`} imageOne={item.image.desktop?.url} />
-						)
+							<FlipCard
+								widthImage={item.image.desktop.width}
+								heightImage={310}
+								titleFlip={item.title}
+								options={item.options}
+								key={`${index}--flips--cards`}
+								imageOne={item.image.desktop?.url}
+							/>
+						);
 					})}
 				</div>
 			</Container>
 			<div className="w-full flex justify-center p-4 mb-5">
-				<button onClick={handleChangeUser} className="button-start font-bold text-black">Começar</button>
+				<button
+					onClick={handleChangeUser}
+					className="button-start font-bold text-black"
+				>
+					Começar
+				</button>
 			</div>
 			<Container>
 				<Conceitos />
 			</Container>
 			<Container>
 				<BannerCarouselBlock />
-				<p className="text-center text-black p-12 m-0 text-2xl  font-bold">QUERIDINHOS DO MOMENTO</p>
-				<BestSellerProductFeed data={darlingState} isLoading={isLoading} error={error} />
+				<p className="text-center text-black p-12 m-0 text-2xl  font-bold">
+					QUERIDINHOS DO MOMENTO
+				</p>
+				<BestSellerProductFeed
+					data={darlingState}
+					isLoading={isLoading}
+					error={error}
+				/>
 
 				<Container className="mb-14">
-					<p className='text-center w-full p-4 text-center text-black font-bold text-2xl'>FAVORITOS Re.You</p>
+					<p className="text-center w-full p-4 text-center text-black font-bold text-2xl">
+						FAVORITOS Re.You
+					</p>
 				</Container>
-				<BestSellerProductFeed data={dataFavorites} isLoading={isLoadDFavorites} error={errorFavorites} />
+				<BestSellerProductFeed
+					data={dataFavorites}
+					isLoading={isLoadDFavorites}
+					error={errorFavorites}
+				/>
 
-				<BrandGridBlock dataBrands={dataBrands} sectionHeading="NOVIDADES NA Re.You" />
+				<BrandGridBlock
+					dataBrands={dataBrands}
+					sectionHeading="NOVIDADES NA Re.You"
+				/>
 
 				{!isEmpty(dataConfigPortal.urlVideoInst) && (
-					<div className='flex items-center flex-col justify-center container-video-inst my-12 w-full mx-auto py-4 px-20 bg-gray-300 text-center'>
-						<p className='text-xl font-bold text-black p-8' >QUER CONHECER A NOSSA HISTÓRIA?</p>
-						<div className='my-4 w-full h-full bg-gray-200'>
-							<iframe src='https://www.youtube.com/embed/E7wJTI-1dvQ'
-								frameBorder='0'
-								allow='autoplay; encrypted-media'
+					<div className="flex items-center flex-col justify-center container-video-inst my-12 w-full mx-auto py-4 px-20 bg-gray-300 text-center">
+						<p className="text-xl font-bold text-black p-8">
+							QUER CONHECER A NOSSA HISTÓRIA?
+						</p>
+						<div className="my-4 w-full h-full bg-gray-200">
+							<iframe
+								src="https://www.youtube.com/embed/E7wJTI-1dvQ"
+								frameBorder="0"
+								allow="autoplay; encrypted-media"
 								allowFullScreen
-								width='100%'
-								height='100%'
-								title='video'
+								width="100%"
+								height="100%"
+								title="video"
 							/>
 						</div>
-						<Link href='/sobre-nos'>
-							<button className='text-black font-bold my-4 w-40 p-4 bg-gray-300  shadow-cardMoreContent rounded-full focus:outline-none hover:bg-gray-400 hover:text-white transition-all '>
+						<Link href="/sobre-nos">
+							<button className="text-black font-bold my-4 w-40 p-4 bg-gray-300  shadow-cardMoreContent rounded-full focus:outline-none hover:bg-gray-400 hover:text-white transition-all ">
 								Saiba Mais
 							</button>
 						</Link>
 					</div>
 				)}
-				<Container className='bg-more_content pb-16 shadow-cardMoreContent'>
+				<Container className="bg-more_content pb-16 shadow-cardMoreContent">
 					<div>
-						<p className='font-bold text-black text-xl py-12 text-center'>MAIS CONTEÚDO PRA VOCÊ</p>
+						<p className="font-bold text-black text-xl py-12 text-center">
+							MAIS CONTEÚDO PRA VOCÊ
+						</p>
 					</div>
-					<div className='flex justify-between xs:flex-col lg:gap-10'>
+					<div className="flex justify-between xs:flex-col lg:gap-10">
 						{dataBlogs.map((item: BlogsType, index) => {
 							return (
-								<div key={index} className=' sm:w-[150px] md:w-[280px] lg:w-[410px] bg-card_read_more pb-4 shadow-cardMoreContent card_read_more flex items-center flex-col'>
-									<Image className='my-8' src={item.image} width={450} height={350} />
-									<span className='uppercase text-center my-4 font-bold text-black'>{item.title}</span>
-									<p className='m-4 text-center text-black'>{item.text}</p>
-									<button className='text-black font-bold w-40 p-4 bg-button_read my-4 shadow-header rounded-full focus:outline-none hover:bg-gray-400 hover:text-white transition-all '>Leia Mais</button>
+								<div
+									key={index}
+									className=" sm:w-[150px] md:w-[280px] lg:w-[410px] bg-card_read_more pb-4 shadow-cardMoreContent card_read_more flex items-center flex-col"
+								>
+									<Image
+										className="my-8"
+										src={item.image}
+										width={450}
+										height={350}
+									/>
+									<span className="uppercase text-center my-4 font-bold text-black">
+										{item.title}
+									</span>
+									<p className="m-4 text-center text-black">{item.text}</p>
+									<button className="text-black font-bold w-40 p-4 bg-button_read my-4 shadow-header rounded-full focus:outline-none hover:bg-gray-400 hover:text-white transition-all ">
+										Leia Mais
+									</button>
 								</div>
-							)
+							);
 						})}
 					</div>
 				</Container>
 
-				<Container className='bg-gray-900 shadow-cardMoreContent pb-20 my-20'>
+				<Container className="bg-gray-900 shadow-cardMoreContent pb-20 my-20">
 					<div>
-						<p className='font-bold text-black text-xl py-12 text-center'>Re.You na IMPRENSA</p>
+						<p className="font-bold text-black text-xl py-12 text-center">
+							Re.You na IMPRENSA
+						</p>
 					</div>
-					<div className='flex gap-10'>
-						<div className='w-2/6 p-12 text-black bg-card_imprensa flex items-center flex-col'>
-							<span className='my-4 text-center font-bold '>22/07/2021 - Nota da Re.YOU ao GreenPeace</span>
-							<p className='m-4 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo laudantium vel distinctio culpa aspernatur quasi. Doloremque ut nobis, optio cumque illo explicabo autem culpa facilis temporibus esse minus iure eligendi!</p>
-						</div>
-						<div className='w-2/6 p-12 text-center text-black  bg-card_imprensa flex items-center flex-col'>
-							<span className='font-bold my-4 '>22/07/2021 - Globo reconhece Re.YOU como a plataforma de Moda Sustentável mais rentável do Brasil</span>
-							<p className='m-4 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo laudantium vel distinctio culpa aspernatur quasi. Doloremque ut nobis, optio cumque illo explicabo autem culpa facilis temporibus esse minus iure eligendi!</p>
-						</div>
-						<div className=' w-2/6 p-12 text-black text-center bg-card_imprensa flex items-center flex-col'>
-							<span className=' font-bold my-4 '>21/07/2021 - CNN entrevista equipe Re.YOU em noite descontraída</span>
-							<iframe src='https://www.youtube.com/embed/E7wJTI-1dvQ'
-								frameBorder='0'
-								allow='autoplay; encrypted-media'
-								allowFullScreen
-								width='100%'
-								height='100%'
-								title='video'
-							/>
-						</div>
+					<div className="flex flex-wrap gap-8">
+						{dataPress.map((item: any) => {
+							return (
+								<>
+									<div className="cards-press p-10 bg-card_imprensa  text-black flex items-center flex-col">
+										<span className="my-4 text-center font-bold ">
+											{item.title}
+										</span>
+										<p className="m-4 text-center">
+											{item.text}
+										</p>
+									</div>
+									{item.sourceUrl && (
+										<div className=" w-2/6 p-12 text-black text-center bg-card_imprensa flex items-center flex-col">
+											<span className=" font-bold my-4 ">
+												{item.title}
+											</span>
+											<iframe
+												src="https://www.youtube.com/embed/E7wJTI-1dvQ"
+												frameBorder="0"
+												allow="autoplay; encrypted-media"
+												allowFullScreen
+												width="100%"
+												height="100%"
+												title="video"
+											/>
+										</div>
+									)}
+								</>
+							);
+						})}
 					</div>
 				</Container>
-				{dataConfigPortal?.app?.showAppDownload && <DownloadApps className="mb-10" />}
+				{dataConfigPortal?.app?.showAppDownload && (
+					<DownloadApps className="mb-10" />
+				)}
 			</Container>
 			<Container>
 				{/* <Subscription className="bg-opacity-0 px-5 sm:px-16 xl:px-0 py-12 md:py-14 xl:py-16" /> */}
